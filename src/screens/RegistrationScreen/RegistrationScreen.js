37703de4936af61b830a21390e7636ca6287ab68
PraveenM24 <praveen050800@gmail.com>
2021-05-20 11:42:33 +0530

    Email Verification

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -24,6 +24,7 @@ export default function RegistrationScreen({navigation}) {
             .auth()
             .createUserWithEmailAndPassword(email, password)
             .then((response) => {
+                response.user.sendEmailVerification();
                 const uid = response.user.uid
                 const data = {
                     id: uid,

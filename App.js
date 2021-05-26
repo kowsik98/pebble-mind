8c085e6ed1e553814567e75000b6b36811970a66
kowsik98 <kowsik5798@gmail.com>
2021-05-26 19:30:16 +0530

    update

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import 'react-native-gesture-handler';
 import React, { useEffect, useState } from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
-import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens'
+
+import { LoginScreen, HomeScreen, RegistrationScreen,DetailsScreen,ProfileScreen} from './src/screens'
 import {decode, encode} from 'base-64'
 if (!global.btoa) {  global.btoa = encode }
 if (!global.atob) { global.atob = decode }
@@ -12,20 +13,26 @@ const Stack = createStackNavigator();
 export default function App() {
 
   const [loading, setLoading] = useState(true)
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState('abc')
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
         { user ? (
+          <>
           <Stack.Screen name="Home">
             {props => <HomeScreen {...props} extraData={user} />}
           </Stack.Screen>
+          <Stack.Screen name="Details" component={DetailsScreen} />
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          </>  
+          
         ) : (
           <>
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Registration" component={RegistrationScreen} />
             <Stack.Screen name="Home" component={HomeScreen} />
+            <Stack.Screen name="Details" component={DetailsScreen} />                  
           </>
         )}
       </Stack.Navigator>
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,10 +1,119 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import 'react-native-gesture-handler';
+import React from 'react';
+import { Text, View, Button} from 'react-native';
+import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view'
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import ProfileScreen from '../ProfileScreen/ProfileScreen';
 
-export default function HomeScreen(props) {
+function Feed() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+function Doctors() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Doctors!</Text>
+    </View>
+  );
+}
+
+
+function MedicalHistory() {
+  return (
+    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>MedicalHistory</Text>
+    </View>
+  );
+}
+function Profile() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Profile!</Text>
+    </View>
+  );
+}
+
+
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Feed"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="Feed"
+        component={Feed}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Doctors"
+        component={Doctors}
+        options={{
+          tabBarLabel: 'Doctors',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="MedicalHistory"
+        component={MedicalHistory}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+        
+      />
+       <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+        
+      />
+     
+      
+    </Tab.Navigator>
+  );
+}
+export default function HomeScreen({navigation}) {
+
     return (
-        <View>
+        /*<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
-        </View>
+            <Button
+              title="Go to Details"
+              onPress={() => navigation.navigate('Details')}
+            />
+            <Button
+            title="profile"
+            onPress={()=> navigation.navigate('Profile')}
+            />
+        </View>*/
+    
+      <MyTabs />
+    
     )
 }

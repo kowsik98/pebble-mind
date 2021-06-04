ab9037ab693005767d985e4e335b54a4e1135e0b
Praveen M <69027747+PraveenM24@users.noreply.github.com>
2021-06-04 12:43:47 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDIFPvyNT5z72rHr1srQjBUZaZmWUuzRgQ",
-    databaseURL: "https://pebble-d2835-default-rtdb.asia-southeast1.firebasedatabase.app",
-    authDomain: "pebble-d2835.firebaseapp.com",
-    projectId: "pebble-d2835",
-    storageBucket: "pebble-d2835.appspot.com",
-    messagingSenderId: "236624507943",
-    appId: "1:236624507943:web:4e729b4257cbb73dc20a02",
-    measurementId: "G-T3DG8HHH48"
+  apiKey: "AIzaSyDIFPvyNT5z72rHr1srQjBUZaZmWUuzRgQ",
+  authDomain: "pebble-d2835.firebaseapp.com",
+  databaseURL: "https://pebble-d2835-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "pebble-d2835",
+  storageBucket: "pebble-d2835.appspot.com",
+  messagingSenderId: "236624507943",
+  appId: "1:236624507943:web:62a60190c94817dec20a02",
+  measurementId: "G-JBQB4XW21B"
 };
 
 
